ee53c6d8ad9be0f2193bcfe8671e57830ab20d1d
ppoi <ppoi@tsukuba-bunko.org>
2013-04-09 22:42:39 +0900

    [機能 #84] スレ画面の改善
    * 元スレURLを表示
    * アーカイブ済みの場合レス数をイタリックで表示
    * パフォーマンス検証用にラップタイムを表示
    * フッターは更新機能の実装時に合わせて作成する
    
    fixes #84

--- public/js/hina.js
+++ public/js/hina.js
@@ -84,41 +84,66 @@ function handle_threadlist_pageshow(event, data) {
 }
 
 function handle_thread_pagebeforeshow(event, dat) {
-  $('div#threadview').text('にゃー');
+  $('#threadview').empty();
+}
+
+function render_thread_posts(posts, threadview) {
+  if(!threadview) {
+    threadview = $('#threadview');
+  }
+
+  for(var i = 0; i < posts.length; ++i) {
+    var post = posts[i];
+    threadview.append($('<div id="' + post.key + '" class="post"/>').append(
+      $('<div class="post-header"/>').append(
+        $('<span class="post-number"/>').text(String(i + 1)),
+        $('<span class="post-author"/>').text(post.author),
+        $('<span class="post-mail"/>').text(post.mail ? post.mail : ''),
+        $('<br/>'),
+        $('<span class="post-date"/>').text(post.post_date),
+        $('<span class="post-hash"/>').text(post.author_hash)
+      ),
+      $('<div class="post-content"/>').html(post.contents)
+    ));
+  }
 }
 
 function handle_thread_pageshow(event, dat) {
   var url = $.mobile.path.parseUrl(location.href)
   var thread_id = url.hash.substring(8);
   var page = $(event.target);
+  var view = $('#threadview');
+  var log = $('#threadview-log');
+  if(log.length == 0) {
+    log = $('<div id="threadview-log">').appendTo(view);
+  }
+
   $.mobile.loading('show', { text:'Loading', textVisible:true });
+  var lap1 = new Date();
   $.ajax('thread/' + thread_id, {
   }).done(function(data, textStatus, jqXHR) {
-    var view = $('#threadview');
-    view.append($('<div class="thread-header"/>').append(
-      $('<h1/>').text(data.title),
+    var lap2 = new Date();
+    var networkCost = lap2.getTime() - lap1.getTime();
+    log.append($('<span class="threadview-log-network"/>').text(networkCost));
+    var post_count_class = data.archived ? 'post-count post-archived' : 'post-count';
+    view.append($('<div class="thread-header ui-body ui-body-d"/>').append(
+      $('<h1 class="thread-title"/>').text(data.title),
       $('<div class="thread-info"/>').append(
-        $('<span class="post-count"/>').text(String(data.post_count)),
+        $('<a class="thread-source-url" href="' + data.source_url + '"/>').text(data.source_url),
+        $('<br/>'),
+        $('<span class="' + post_count_class + '"/>').text(String(data.post_count)),
+        $('<br/>'),
         $('<span class="thread-period"/>').append(
           $('<span class="thread-created-date"/>').text(data.created_date),
           $('<span class="thread-lastpost-date"/>').text(data.lastpost_date)
         )
       )
     ));
-    for(var i = 0; i < data.posts.length; ++i) {
-      var post = data.posts[i];
-      view.append($('<div class="post"/>').append(
-        $('<div class="post-header"/>').append(
-          $('<span class="post-number"/>').text(String(i + 1)),
-          $('<span class="post-author"/>').text(post.author),
-          $('<span class="post-mail"/>').text(post.mail ? post.mail : ''),
-          $('<br/>'),
-          $('<span class="post-date"/>').text(post.post_date),
-          $('<span class="post-hash"/>').text(post.author_hash)
-        ),
-        $('<div class="post-content"/>').html(post.contents)
-      ));
-    }
+    render_thread_posts(data.posts, view);
+    var lap3 = new Date();
+    var renderCost = lap3.getTime() - lap2.getTime();
+    log.append($('<span class="threadview-log-render"/>').text(renderCost));
+    $.mobile.silentScroll();
     $.mobile.loading('hide');
   }).fail(function(jqXHR, textStatus, errorThrown) {
     window.alert(errorThrown);
